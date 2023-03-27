42c9b30aa2a76378580d20f3e3b24a088f99adca
Your Name <1758555877@qq.com>
2023-03-27 17:48:37 +0800

    学习表单验证

---
+++ js2/form.js
@@ -0,0 +1,8 @@
+function validateForm() {        
+    var x=document.forms["myForm"]["fname"].value;        
+    if (x==null || x==""){        
+        alert("First name must be filled out");        
+        return false;        
+    }        
+}
+// 表单验证
